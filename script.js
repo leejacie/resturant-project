e75a6313d8ff95f477a9a9231ada970b2f877a04
leejacie <110567022+leejacie@users.noreply.github.com>
2023-01-06 21:41:07 -1000

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,27 @@
     var popup = document.getElementById("pub");
     popup.classList.toggle("show");
   }
+let counterDisplayElem = document.querySelector('.counter-display');
+let counterMinusElem = document.querySelector('.counter-minus');
+let counterPlusElem = document.querySelector('.counter-plus');
+
+let count = 0;
+
+updateDisplay();
+
+counterPlusElem.addEventListener("click",()=>{
+    count++;
+    updateDisplay();
+}) ;
+
+counterMinusElem.addEventListener("click",()=>{
+    count--;
+    updateDisplay();
+});
+
+function updateDisplay(){
+    counterDisplayElem.innerHTML = count;
+};
   function sandopu() {
     var popup2 = document.getElementById("puSando");
     popup2.classList.toggle("show2");
