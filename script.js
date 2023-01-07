2581c5578a5e0a80ecfc67b899b3090412a04a58
leejacie <110567022+leejacie@users.noreply.github.com>
2023-01-06 19:37:06 -1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+  function bearpu() {
+    var popup = document.getElementById("pub");
+    popup.classList.toggle("show");
+  }
+  function sandopu() {
+    var popup2 = document.getElementById("puSando");
+    popup2.classList.toggle("show2");
+  }
+  function bingspu() {
+    var popup3 = document.getElementById("pubing");
+    popup3.classList.toggle("show3");
+  }
+  function lpu() {
+    var popup4 = document.getElementById("pulat");
+    popup4.classList.toggle("show4");
+  }
+  function milkpu() {
+    var popup5 = document.getElementById("pumilk");
+    popup5.classList.toggle("show5");
+  }
+  function parfpu() {
+    var popup6 = document.getElementById("puparf");
+    popup6.classList.toggle("show6");
+  }
