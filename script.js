c11b03276f4b23db4bab16fefbfdd07594f8f354
leejacie <110567022+leejacie@users.noreply.github.com>
2023-01-15 14:29:47 -1000

    Update script.js

--- script.js
+++ script.js
@@ -2,97 +2,48 @@ function bearpu() {
   var popup = document.getElementById("pub");
   popup.classList.toggle("show");
 }
-function numbers(){
-var select = document.getElementById("selectNumber"); 
-var options = ["0", "1", "2", "3", "4", "5"]; 
 
-    for(var i = 0; i < options.length; i++) {
-        var opt = options[i];
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        select.appendChild(el);
+function add(){
+  let x = document.getElementById("quantity").value;
+  //set the variable x to local storage with the key of firstname
+  localStorage.setItem("bmount", x);
+  //clearing the input after the button is clicked
+  document.getElementById("quantity").value = " ";
+
+  let b = document.getElementById("bname").value;
+        //set the variable x to local storage with the key of firstname
+  localStorage.setItem("bbn", b);
+  
+  let bp = document.getElementById("bprice").value;
+  localStorage.setItem("bbp",bp);
 }
-}
+
 
 function sandopu() {
   var popup2 = document.getElementById("puSando");
   popup2.classList.toggle("show2");
-}
-function numbers2(){
-  var select = document.getElementById("selectNumber2"); 
-var options = ["0", "1", "2", "3", "4", "5"]; 
-
-    for(var i = 0; i < options.length; i++) {
-        var opt = options[i];
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        select.appendChild(el);
-}
 }
 
 function bingspu() {
   var popup3 = document.getElementById("pubing");
   popup3.classList.toggle("show3");
 }
-function numbers3(){
-  var select = document.getElementById("selectNumber3"); 
-  var options = ["0", "1", "2", "3", "4", "5"]; 
 
-    for(var i = 0; i < options.length; i++) {
-        var opt = options[i];
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        select.appendChild(el);
-}
-}
 function lpu() {
   var popup4 = document.getElementById("pulat");
   popup4.classList.toggle("show4");
 }
-function numbers4(){
-  var select = document.getElementById("selectNumber4"); 
-  var options = ["0", "1", "2", "3", "4", "5"]; 
 
-    for(var i = 0; i < options.length; i++) {
-        var opt = options[i];
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        select.appendChild(el);
-}
-}
 function milkpu() {
   var popup5 = document.getElementById("pumilk");
   popup5.classList.toggle("show5");
 }
-function numbers5(){
-  var select = document.getElementById("selectNumber5"); 
-  var options = ["0", "1", "2", "3", "4", "5"]; 
 
-    for(var i = 0; i < options.length; i++) {
-        var opt = options[i];
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        select.appendChild(el);
-}
-}
 function parfpu() {
   var popup6 = document.getElementById("puparf");
   popup6.classList.toggle("show6");
 }
-function numbers6(){
-  var select = document.getElementById("selectNumber6"); 
-  var options = ["0", "1", "2", "3", "4", "5"]; 
 
-    for(var i = 0; i < options.length; i++) {
-        var opt = options[i];
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        select.appendChild(el);
+function pay(){
+  location.href="check.html";
 }
-  }
